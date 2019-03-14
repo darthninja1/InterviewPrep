ec5ae5ccd2a09789b62f6230d8c405674c2c76d5
darthninja1 <nmalik1@gmail.com>
2019-03-14 19:07:28 -0400

    marathon winners problem statement

--- src/interviews/MarathonWinners.java
+++ src/interviews/MarathonWinners.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array A that has indices of marathon winners
+ * There are no ties
+ * A[i] = j means that “i” follows “j” i.e. j is ahead of i.
+ * A[i] = -1 means that “i” is the winner.
+ * Examples:
+ * [1, 3, 0, -1] should print [3,1,0,2]
+ * [4,5,0,-1,1,3] should print [3,5,1,4,0,2]
+ */
 public class MarathonWinners {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getPositions(new int[]{1, 3, 0, -1})));

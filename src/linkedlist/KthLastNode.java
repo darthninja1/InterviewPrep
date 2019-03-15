e2f54ea31787272abae745be5d0fbc2b19d3a680
darthninja1 <nmalik1@gmail.com>
2019-03-15 18:52:58 -0400

    linkedlist kth last node

--- src/linkedlist/KthLastNode.java
+++ src/linkedlist/KthLastNode.java
@@ -6,34 +6,34 @@ import static linkedlist.SinglyLinkedList.forwardTraversalRecursive;
 public class KthLastNode {
     public static void main(String[] args) {
         ListNode head = createList();
-        System.out.println("--> " + getKthLastNode(head, 4));
-        System.out.println("--> " + getKthLastNode(head, 3));
-        System.out.println("--> " + getKthLastNode(head, 2));
-        System.out.println("--> " + getKthLastNode(head, 1));
-        System.out.println("--> " + getKthLastNode(head, 5));
-        System.out.println("--> " + getKthLastNode(new ListNode(10), 1));
-        System.out.println("--> " + getKthLastNode(new ListNode(10).setNext(new ListNode(11)), 2));
+        getKthLastNode(head, 4);
+        getKthLastNode(head, 3);
+        getKthLastNode(head, 2);
+        getKthLastNode(head, 1);
+        getKthLastNode(head, 5);
+        getKthLastNode(new ListNode(10), 1);
+        getKthLastNode(new ListNode(10).setNext(new ListNode(11)), 2);
     }
 
-    private static int getKthLastNode(ListNode head, int k) {
+    private static void getKthLastNode(ListNode head, int k) {
         forwardTraversalRecursive(head);
-        if (head == null || head.next == null) return Integer.MIN_VALUE;
         ListNode ptr = head;
-        int count = 1;
-        while (ptr.next != null) {
-            count++;
-            ptr = ptr.next;
+        if (head == null) {
+            System.out.println(k +" --> Error");
+            return;
         }
-        if (k > count) return Integer.MIN_VALUE;
         ListNode kptr = head;
         for (int i = 0; i < k; i++) {
+            if (kptr == null) {
+                System.out.println(k +" --> Error");
+                return;
+            }
             kptr = kptr.next;
         }
-        ptr = head;
         while (kptr != null) {
             ptr = ptr.next;
             kptr = kptr.next;
         }
-        return ptr.value;
+        System.out.println(k + " --> " + ptr.value);
     }
 }

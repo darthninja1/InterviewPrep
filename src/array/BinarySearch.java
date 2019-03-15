f74e951bc10fe7d72602f4c21e26641331f28a66
darthninja1 <nmalik1@gmail.com>
2019-03-15 16:18:42 -0400

    binary search

---
+++ src/array/BinarySearch.java
@@ -0,0 +1,28 @@
+package array;
+
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(search(array, 5));
+        System.out.println(search(array, 9));
+        System.out.println(search(array, 1));
+    }
+
+    private static boolean search(final int[] array, int num) {
+        int startIndex = 0;
+        int endIndex = array.length - 1;
+        while (startIndex <= endIndex) {
+            int mid = (endIndex + startIndex) / 2;
+            if (array[mid] > num) {
+                endIndex = mid - 1;
+            } else if (array[mid] < num) {
+                startIndex = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}

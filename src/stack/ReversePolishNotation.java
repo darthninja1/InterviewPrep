9b0ff50762a42cdc470dd53faed22963bf8c9f68
darthninja1 <nmalik1@gmail.com>
2019-03-16 11:44:26 -0400

    Reverse polish notation - naive and flexible solution to support new operators without changing existing code.

---
+++ src/stack/ReversePolishNotation.java
@@ -0,0 +1,139 @@
+package stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+
+public class ReversePolishNotation {
+    static HashMap<String, RPNOperator> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"1", "2", "+", "4", "*"};
+        String[] arr2 = new String[]{"1", "-", "2", "+", "4", "*"};
+        System.out.println(computeRPNNaive(arr));
+
+        registerOperator(new Add());
+        registerOperator(new Subtract());
+        registerOperator(new Multiply());
+        registerOperator(new Divide());
+        System.out.println(computeRPN2(arr));
+        System.out.println(computeRPN2(arr2));
+    }
+
+    private static int computeRPNNaive(String[] arr) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String s : arr) {
+            if (!"+-*/".contains(s)) {
+                stack.push(Integer.valueOf(s));
+            } else {
+                int rightOp = stack.pop();
+                int leftOp = stack.pop();
+                switch (s) {
+                    case "+":
+                        stack.push(rightOp + leftOp);
+                        break;
+                    case "-":
+                        stack.push(rightOp - leftOp);
+                        break;
+                    case "*":
+                        stack.push(rightOp * leftOp);
+                        break;
+                    case "/":
+                        if (leftOp == 0) throw new IllegalArgumentException("divide by zero");
+                        stack.push(rightOp / leftOp);
+                        break;
+                    default:
+                        throw new RuntimeException("error");
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    private static void registerOperator(RPNOperator op) {
+        map.put(op.getOperator(), op);
+    }
+
+    private static int computeRPN2(String[] arr) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String s : arr) {
+            if (!map.containsKey(s)) {
+                stack.push(Integer.valueOf(s));
+            } else {
+                map.get(s).compute(stack);
+            }
+        }
+        return stack.pop();
+    }
+
+    interface RPNOperator {
+        String getOperator();
+
+        void compute(Deque<Integer> stack);
+    }
+
+    static class Add implements RPNOperator {
+
+        @Override
+        public String getOperator() {
+            return "+";
+        }
+
+        @Override
+        public void compute(Deque<Integer> stack) {
+            int rightOp = stack.pop();
+            int leftOp = stack.pop();
+            stack.push(rightOp + leftOp);
+        }
+    }
+
+    static class Subtract implements RPNOperator {
+
+        @Override
+        public String getOperator() {
+            return "-";
+        }
+
+        @Override
+        public void compute(Deque<Integer> stack) {
+            int rightOp = stack.pop();
+            if (stack.peek() != null) {
+                int leftOp = stack.pop();
+                stack.push(rightOp - leftOp);
+            } else { // unary
+                stack.push(0 - rightOp);
+            }
+        }
+    }
+
+    static class Multiply implements RPNOperator {
+
+        @Override
+        public String getOperator() {
+            return "*";
+        }
+
+        @Override
+        public void compute(Deque<Integer> stack) {
+            int rightOp = stack.pop();
+            int leftOp = stack.pop();
+            stack.push(rightOp * leftOp);
+        }
+    }
+
+    static class Divide implements RPNOperator {
+
+        @Override
+        public String getOperator() {
+            return "/";
+        }
+
+        @Override
+        public void compute(Deque<Integer> stack) {
+            int rightOp = stack.pop();
+            int leftOp = stack.pop();
+            if (leftOp == 0) throw new IllegalArgumentException("divide by zero");
+            stack.push(rightOp / leftOp);
+        }
+    }
+}

d4d296d0843dee693b90d8bbc6af988cebccc0db
darthninja1 <nmalik1@gmail.com>
2019-03-21 21:55:21 -0400

    Update pangram check to return missing chars

--- src/string/Pangram.java
+++ src/string/Pangram.java
@@ -1,17 +1,17 @@
 package string;
 
 // https://www.hackerrank.com/challenges/pangrams/problem
+// https://github.com/rajanmalhotra/Code-Samples/blob/master/twosigma/PangramCheck.java
 public class Pangram {
     public static void main(String[] args) {
+        System.out.println(pangram(null));
         System.out.println(pangram("hello"));
         System.out.println(pangram("We promptly judged antique ivory buckles for the prize"));
         System.out.println(pangram("We promptly judged antique ivory buckles for the next prize"));
     }
 
     static String pangram(String s) {
-        if (s == null || s.length() < 26) {
-            return "not pangram";
-        }
+        if (s == null) s = "";
         int[] chars = new int[26];
         char[] s1 = s.toLowerCase().toCharArray();
         for (int i = 0; i < s1.length; i++) {
@@ -20,11 +20,12 @@ public class Pangram {
             }
             chars[s1[i] - 'a']++;
         }
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == 0) {
-                return "not pangram";
+                sb.append((char) (i + 'a'));
             }
         }
-        return "pangram";
+        return (sb.length() == 0) ? "pangram" : sb.toString();
     }
 }

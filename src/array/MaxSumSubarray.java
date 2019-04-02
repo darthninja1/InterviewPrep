c3e159979dc9d0815940227fb3582a6c64a77058
darthninja1 <nmalik1@gmail.com>
2019-04-02 18:31:40 -0400

    Clean up

--- src/array/MaxSumSubarray.java
+++ src/array/MaxSumSubarray.java
@@ -1,7 +1,7 @@
 package array;
 
 // https://leetcode.com/problems/maximum-subarray/
-public class MaximumSumSubarray {
+public class MaxSumSubarray {
     public static void main(String[] args) {
         System.out.println(maxSum(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         System.out.println(maxSum(new int[]{-2, -1, -3, -4, -1, -2, -1, -5, -4}));

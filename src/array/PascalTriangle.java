0bff4f50e0543379f6715adf2463b71875034601
darthninja1 <nmalik1@gmail.com>
2019-04-15 19:26:47 -0400

    Pascal's Triangle

--- src/array/PascalTriangle.java
+++ src/array/PascalTriangle.java
@@ -38,7 +38,7 @@ public class PascalTriangle {
 
     private static void print(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();

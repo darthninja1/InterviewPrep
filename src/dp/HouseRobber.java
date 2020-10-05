f63957a6f6ca22723d70aee8ae89a7fd2a97c4a8
darthninja1 <nmalik1@gmail.com>
2020-10-04 20:00:32 -0400

    House Robber

---
+++ src/dp/HouseRobber.java
@@ -0,0 +1,27 @@
+package dp;
+
+// https://leetcode.com/problems/house-robber/
+public class HouseRobber {
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+        System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
+        System.out.println(rob(new int[]{6, 7, 1, 3, 8, 2, 4}));
+    }
+
+    private static int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[dp.length - 1];
+    }
+}
